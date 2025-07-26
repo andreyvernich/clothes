7d0e204a9096e1e570035ae1ba5ae0691a379a53
Andrey <andrey.vernich@yandex.ru>
2025-07-26 20:45:37 +0300

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Clothes> shmotki = new ArrayList<>();
 
         Clothes skirt = new Skirt(Size.XS, new BigDecimal(200), "pink");
         Clothes teeShirt = new TeeShirt(Size.S, new BigDecimal(1000), "brown");
         Clothes tie = new Tie(Size.M, new BigDecimal(500), "white");
         Clothes trousers = new Trousers(Size.L, new BigDecimal(300), "blue");
 
+        List<Clothes> shmotki = new ArrayList<>();
         shmotki.add(skirt);
         shmotki.add(teeShirt);
         shmotki.add(tie);
